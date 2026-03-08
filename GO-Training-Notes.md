1cdeb960e326bc72b820056a0f2acf6078c8cfce
jmcortesm <jmcortesm@kognivera.com>
2026-03-07 18:24:58 -0600

    Update

--- GO-Training-Notes.md
+++ GO-Training-Notes.md
@@ -1,5 +1,8 @@
 # Go Training Notes
 
-Finish Udemy Course
-
-Create micro projects
+1. Do Udemy Course
+2. Learn Go the Hard Way
+   1. https://learncodethehardway.com/client/#/course/2180/
+3. Do Programming in Go Course
+   1. https://www.youtube.com/watch?v=iDQAZEJK8lI&list=PLoILbKo9rG3skRCj37Kn5Zj803hhiuRK6
+4. Create micro projects
--- Go-Udemy-Course/Section-6-slice-map-range/1-Slice-Part-I.go
+++ Go-Udemy-Course/Section-6-slice-map-range/1-Slice-Part-I.go
@@ -6,3 +6,20 @@ func main() {
 	// Slice is a reference type, it does not store any data, it just points to an underlying array.
 	// The length of a slice is the number of elements in the slice, while the capacity is the number of elements in the underlying array.
 	// When you append to a slice and it exceeds its capacity, a new underlying array is created and the existing elements are copied to it.
+
+	// Empty slice
+	keywords := []string{}
+	fmt.Println(keywords)
+
+	// Slice with values
+	src := []string{"x", "y", "z"}
+	fmt.Println(src)
+
+	// get/set values by index
+	fmt.Println(src[0]) // x
+	src[1] = "y"
+
+	// extend a slice append new values to it
+	src = append(src, "a", "b", "c")
+	fmt.Println(src)
+}
